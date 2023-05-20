5899c2d9936516b3102379b9e75a046d3bbbbf8b
givesilverstick <silverstick21@sookmyung.ac.kr>
2023-05-20 17:23:59 +0900

    퀴즈 맞췄을 경우 - 포인트 증가 반영

--- app/src/main/java/smu/it/ips2/CorrectActivity.kt
+++ app/src/main/java/smu/it/ips2/CorrectActivity.kt
@@ -4,11 +4,52 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class CorrectActivity : AppCompatActivity() {
+    private var auth: FirebaseAuth = FirebaseAuth.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_correct)
+
+        val database = FirebaseDatabase.getInstance()
+
+        val currentUser = auth.currentUser
+        val userId = currentUser?.uid
+        val ref = database.getReference("users").child(userId.toString()).child("point")
+
+        // 데이터 읽기
+        ref.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val currentValue = dataSnapshot.getValue(String::class.java) // 현재 값 가져오기
+
+                // 현재 값에서 변화시킬 작업 수행
+                val newValue = (currentValue?.toIntOrNull() ?: 0) + 30 // 현재 값에 30 추가
+
+                // 데이터 수정
+                ref.setValue(newValue.toString())
+                    .addOnSuccessListener {
+                        // 수정 성공
+                        Toast.makeText(this@CorrectActivity, "포인트를 획득했습니다!", Toast.LENGTH_SHORT).show()
+                        // 원하는 작업 수행 or 메시지 표시 가능
+                    }
+                    .addOnFailureListener { exception ->
+                        // 수정 실패
+                        // 오류 처리 수행 or 메시지 표시 가능
+                    }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // 읽기 실패 처리
+                // 오류 처리 수행 or 메시지 표시 가능
+            }
+        })
+
 
         findViewById<ImageButton>(R.id.homeBtn).setOnClickListener {
             intent = Intent(this, MainActivity::class.java)
