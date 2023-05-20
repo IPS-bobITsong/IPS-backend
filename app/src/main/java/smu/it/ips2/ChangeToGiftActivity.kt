f69648d02b4fcec987f16aef154afd4cad51f38a
givesilverstick <silverstick21@sookmyung.ac.kr>
2023-05-20 18:45:07 +0900

    상품으로 교환하기 페이지 - 현재 사용자의 이름/포인트 보이게 설정

--- app/src/main/java/smu/it/ips2/ChangeToGiftActivity.kt
+++ app/src/main/java/smu/it/ips2/ChangeToGiftActivity.kt
@@ -4,11 +4,55 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class ChangeToGiftActivity : AppCompatActivity() {
+    private var auth: FirebaseAuth = FirebaseAuth.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_change_to_gift)
+
+        val currentUser = auth.currentUser
+        val userId = currentUser?.uid
+        val database = FirebaseDatabase.getInstance()
+
+        database.getReference("users/$userId/name").addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val userName = snapshot.getValue(String::class.java)
+
+                // username TextView 가져오기
+                val usernameTextView = findViewById<TextView>(R.id.username)
+
+                // 사용자 이름 설정
+                usernameTextView.text = userName
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // 데이터베이스에서 값 가져오기 실패
+                // 처리 방법 결정 or 오류 처리 가능
+            }
+        })
+
+        database.getReference("users/$userId/point").addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val userPoint = snapshot.getValue(String::class.java)
+
+                val userPointTextView = findViewById<TextView>(R.id.point)
+
+                // 사용자 이름 설정
+                userPointTextView.text = userPoint
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // 데이터베이스에서 값 가져오기 실패
+                // 처리 방법 결정 or 오류 처리 가능
+            }
+        })
 
         findViewById<ImageButton>(R.id.backBtn).setOnClickListener {
             intent = Intent(this, MyPageActivity::class.java)
