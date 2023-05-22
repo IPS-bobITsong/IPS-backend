9722a4b32864be8f3718b5def93a53a6227e7b69
JIMIN <jiminiyooni@sookmyung.ac.kr>
2023-05-22 19:07:41 +0900

    건강기사에 따라 카드뉴스 나타내는 코드 구성(오류 o)

--- app/src/main/java/smu/it/ips2/GainPointActivity.kt
+++ app/src/main/java/smu/it/ips2/GainPointActivity.kt
@@ -1,11 +1,12 @@
 package smu.it.ips2
 
+import android.R
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager2.widget.ViewPager2
+
 
 class GainPointActivity : AppCompatActivity() {
     lateinit var viewPager_article : ViewPager2
@@ -28,6 +29,110 @@ class GainPointActivity : AppCompatActivity() {
             startActivity(intent)
         }
         val article = intent.getStringExtra("quizes")
-        findViewById<TextView>(R.id.example).text =article
+        when (article) {
+            "건강기사 1" -> {
+                viewPager_article.adapter = ViewPagerAdapter(getArticles1())
+                viewPager_article.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+            }
+
+            "건강기사 1" -> {
+                viewPager_article.adapter = ViewPagerAdapter(getArticles1())
+                viewPager_article.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+            }
+
+            "건강기사 2" -> {
+                viewPager_article.adapter = ViewPagerAdapter(getArticles2())
+                viewPager_article.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+            }
+
+            "건강기사 3" -> {
+                viewPager_article.adapter = ViewPagerAdapter(getArticles3())
+                viewPager_article.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+            }
+
+            "건강기사 4" -> {
+                viewPager_article.adapter = ViewPagerAdapter(getArticles4())
+                viewPager_article.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+            }
+
+            "건강기사 5" -> {
+                viewPager_article.adapter = ViewPagerAdapter(getArticles5())
+                viewPager_article.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+            }
+
+            "건강기사 6" -> {
+                viewPager_article.adapter = ViewPagerAdapter(getArticles6())
+                viewPager_article.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+            }
+
+            "건강기사 7" -> {
+                viewPager_article.adapter = ViewPagerAdapter(getArticles7())
+                viewPager_article.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+            }
+
+            "건강기사 8" -> {
+                viewPager_article.adapter = ViewPagerAdapter(getArticles8())
+                viewPager_article.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+            }
+            "건강기사 9" -> {
+                viewPager_article.adapter = ViewPagerAdapter(getArticles8())
+                viewPager_article.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+            }
+        }
     }
+        private fun getArticles1(): ArrayList<Int> {
+            return arrayListOf<Int>(
+                R.drawable.article1_1,)
+            R.drawable.article1_1,)
+            R.drawable.article1_1)
+        }
+        private fun getArticles2(): ArrayList<Int> {
+            return arrayListOf<Int>(
+                R.drawable.article1_1,)
+            R.drawable.article1_1,)
+            R.drawable.article1_1)
+        }
+        private fun getArticles3(): ArrayList<Int> {
+            return arrayListOf<Int>(
+                R.drawable.article1_1,)
+            R.drawable.article1_1,)
+            R.drawable.article1_1)
+        }
+    private fun getArticles4(): ArrayList<Int> {
+        return arrayListOf<Int>(
+            R.drawable.article1_1,)
+        R.drawable.article1_1,)
+        R.drawable.article1_1)
+    }
+    private fun getArticles5(): ArrayList<Int> {
+        return arrayListOf<Int>(
+            R.drawable.article1_1,)
+        R.drawable.article1_1,)
+        R.drawable.article1_1)
+    }
+    private fun getArticles6(): ArrayList<Int> {
+        return arrayListOf<Int>(
+            R.drawable.article1_1,)
+        R.drawable.article1_1,)
+        R.drawable.article1_1)
+    }
+    private fun getArticles7(): ArrayList<Int> {
+        return arrayListOf<Int>(
+            R.drawable.article1_1,)
+        R.drawable.article1_1,)
+        R.drawable.article1_1)
+    }
+    private fun getArticles8(): ArrayList<Int> {
+        return arrayListOf<Int>(
+            R.drawable.article1_1,)
+        R.drawable.article1_1,)
+        R.drawable.article1_1)
+    }
+    private fun getArticles9(): ArrayList<Int> {
+        return arrayListOf<Int>(
+            R.drawable.article1_1,)
+        R.drawable.article1_1,)
+        R.drawable.article1_1)
+    }
+
 }
