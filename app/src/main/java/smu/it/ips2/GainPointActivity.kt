dba45b446f7a546869e2d44945edcd085b6e4ac2
JIMIN <jiminiyooni@sookmyung.ac.kr>
2023-05-22 19:57:48 +0900

    선택한 건강기사에 맞는 건강기사 viewpager로 나타내기

--- app/src/main/java/smu/it/ips2/GainPointActivity.kt
+++ app/src/main/java/smu/it/ips2/GainPointActivity.kt
@@ -1,6 +1,5 @@
 package smu.it.ips2
 
-import android.R
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageButton
@@ -9,13 +8,13 @@ import androidx.viewpager2.widget.ViewPager2
 
 
 class GainPointActivity : AppCompatActivity() {
-    lateinit var viewPager_article : ViewPager2
+    private lateinit var viewPager_article : ViewPager2
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gain_point)
 
         findViewById<ImageButton>(R.id.backBtn).setOnClickListener {
-            intent = Intent(this, MyPageActivity::class.java)
+            intent = Intent(this, GainPointListActivity::class.java)
             startActivity(intent)
         }
 
@@ -28,111 +27,111 @@ class GainPointActivity : AppCompatActivity() {
             intent = Intent(this, QuizActivity::class.java)
             startActivity(intent)
         }
+
+        viewPager_article = findViewById(R.id.vpArticle)
         val article = intent.getStringExtra("quizes")
         when (article) {
             "건강기사 1" -> {
-                viewPager_article.adapter = ViewPagerAdapter(getArticles1())
+                viewPager_article.adapter = ViewPagerAdapterArticle(getArticles1())
                 viewPager_article.orientation = ViewPager2.ORIENTATION_HORIZONTAL
             }
 
             "건강기사 1" -> {
-                viewPager_article.adapter = ViewPagerAdapter(getArticles1())
+                viewPager_article.adapter = ViewPagerAdapterArticle(getArticles1())
                 viewPager_article.orientation = ViewPager2.ORIENTATION_HORIZONTAL
             }
 
             "건강기사 2" -> {
-                viewPager_article.adapter = ViewPagerAdapter(getArticles2())
+                viewPager_article.adapter = ViewPagerAdapterArticle(getArticles2())
                 viewPager_article.orientation = ViewPager2.ORIENTATION_HORIZONTAL
             }
 
             "건강기사 3" -> {
-                viewPager_article.adapter = ViewPagerAdapter(getArticles3())
+                viewPager_article.adapter = ViewPagerAdapterArticle(getArticles3())
                 viewPager_article.orientation = ViewPager2.ORIENTATION_HORIZONTAL
             }
 
             "건강기사 4" -> {
-                viewPager_article.adapter = ViewPagerAdapter(getArticles4())
+                viewPager_article.adapter = ViewPagerAdapterArticle(getArticles4())
                 viewPager_article.orientation = ViewPager2.ORIENTATION_HORIZONTAL
             }
 
             "건강기사 5" -> {
-                viewPager_article.adapter = ViewPagerAdapter(getArticles5())
+                viewPager_article.adapter = ViewPagerAdapterArticle(getArticles5())
                 viewPager_article.orientation = ViewPager2.ORIENTATION_HORIZONTAL
             }
 
             "건강기사 6" -> {
-                viewPager_article.adapter = ViewPagerAdapter(getArticles6())
+                viewPager_article.adapter = ViewPagerAdapterArticle(getArticles6())
                 viewPager_article.orientation = ViewPager2.ORIENTATION_HORIZONTAL
             }
 
             "건강기사 7" -> {
-                viewPager_article.adapter = ViewPagerAdapter(getArticles7())
+                viewPager_article.adapter = ViewPagerAdapterArticle(getArticles7())
                 viewPager_article.orientation = ViewPager2.ORIENTATION_HORIZONTAL
             }
 
             "건강기사 8" -> {
-                viewPager_article.adapter = ViewPagerAdapter(getArticles8())
+                viewPager_article.adapter = ViewPagerAdapterArticle(getArticles8())
                 viewPager_article.orientation = ViewPager2.ORIENTATION_HORIZONTAL
             }
             "건강기사 9" -> {
-                viewPager_article.adapter = ViewPagerAdapter(getArticles8())
+                viewPager_article.adapter = ViewPagerAdapterArticle(getArticles8())
                 viewPager_article.orientation = ViewPager2.ORIENTATION_HORIZONTAL
             }
         }
     }
         private fun getArticles1(): ArrayList<Int> {
             return arrayListOf<Int>(
-                R.drawable.carbohydrate_article1,)
-            R.drawable.carbohydrate_article2,)
-            R.drawable.article1_1)
+                R.drawable.carbohydrate_article1,
+            R.drawable.carbohydrate_article2,
+            R.drawable.carbohydrate_article3)
         }
         private fun getArticles2(): ArrayList<Int> {
             return arrayListOf<Int>(
-                R.drawable.article1_1,)
-            R.drawable.article1_1,)
-            R.drawable.article1_1)
+                R.drawable.protein_article1,
+            R.drawable.protein_article2,
+            R.drawable.protein_article3)
         }
         private fun getArticles3(): ArrayList<Int> {
             return arrayListOf<Int>(
-                R.drawable.article1_1,)
-            R.drawable.article1_1,)
-            R.drawable.article1_1)
+                R.drawable.fat_article1,
+            R.drawable.fat_article2,
+            R.drawable.fat_article3)
         }
     private fun getArticles4(): ArrayList<Int> {
         return arrayListOf<Int>(
-            R.drawable.article1_1,)
-        R.drawable.article1_1,)
-        R.drawable.article1_1)
+            R.drawable.sugars_article1,
+        R.drawable.sugars_article2,
+        R.drawable.sugars_article3)
     }
     private fun getArticles5(): ArrayList<Int> {
         return arrayListOf<Int>(
-            R.drawable.article1_1,)
-        R.drawable.article1_1,)
-        R.drawable.article1_1)
+            R.drawable.natrium_article1,
+        R.drawable.natrium_article2,
+        R.drawable.natrium_article3)
     }
     private fun getArticles6(): ArrayList<Int> {
         return arrayListOf<Int>(
-            R.drawable.article1_1,)
-        R.drawable.article1_1,)
-        R.drawable.article1_1)
+            R.drawable.school_article1,
+        R.drawable.school_article2,
+        R.drawable.school_article3)
     }
     private fun getArticles7(): ArrayList<Int> {
         return arrayListOf<Int>(
-            R.drawable.article1_1,)
-        R.drawable.article1_1,)
-        R.drawable.article1_1)
+            R.drawable.health_article)
     }
     private fun getArticles8(): ArrayList<Int> {
         return arrayListOf<Int>(
-            R.drawable.article1_1,)
-        R.drawable.article1_1,)
-        R.drawable.article1_1)
+            R.drawable.fat_article1,
+        R.drawable.fat_article2,
+        R.drawable.fat_article3)
     }
     private fun getArticles9(): ArrayList<Int> {
         return arrayListOf<Int>(
-            R.drawable.article1_1,)
-        R.drawable.article1_1,)
-        R.drawable.article1_1)
+            R.drawable.brain_article1,
+        R.drawable.brain_article2,
+        R.drawable.brain_article3)
     }
 
 }
