53326c7baed4cb1ecd5499ef7f33a15c15f21338
JIMIN <jiminiyooni@sookmyung.ac.kr>
2023-05-24 04:14:54 +0900

    detailactivity> 영양성분에 따라 메뉴 추천하기 (식단 데이터 x)

--- app/src/main/java/smu/it/ips2/DetailActivity.kt
+++ app/src/main/java/smu/it/ips2/DetailActivity.kt
@@ -3,13 +3,17 @@ package smu.it.ips2
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import androidx.viewpager2.widget.ViewPager2
+import com.google.firebase.firestore.FirebaseFirestore
 
 class DetailActivity : AppCompatActivity() {
 
     lateinit var viewPager_card: ViewPager2
+    var firestore : FirebaseFirestore? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
@@ -28,35 +32,65 @@ class DetailActivity : AppCompatActivity() {
         }
 
 
-            findViewById<ImageButton>(R.id.backBtn).setOnClickListener {
-                intent = Intent(this, CompleteActivity::class.java)
-                startActivity(intent)
+        //파이어스토어 메뉴명&영양성분 불러와서 리스트로 접근
+        val menus = ArrayList<MenuList>()
+        firestore?.collection("restaurantBook")?.addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+            // ArrayList 비워줌
+            menus.clear()
+
+            for (snapshot in querySnapshot!!.documents) {
+                var menuname = snapshot.getString("foodname")
+                var carbo = snapshot.getDouble("carbo")
+                var protein = snapshot.getDouble("protein")
+                var fat = snapshot.getDouble("fat")
+                val menu = MenuList(menuname, carbo, protein, fat)
+                menus.add(menu)
             }
-
-            findViewById<ImageButton>(R.id.mypageBtn).setOnClickListener {
-                intent = Intent(this, MyPageActivity::class.java)
-                startActivity(intent)
-            }
-
-            findViewById<ImageButton>(R.id.homeBtn).setOnClickListener {
-                intent = Intent(this, MainActivity::class.java)
-                startActivity(intent)
-            }
-
-            // 해당 가맹점 링크
-            findViewById<ImageButton>(R.id.buttonRecommend1).setOnClickListener {
-
-            }
-
-            // 해당 가맹점 링크
-            findViewById<ImageButton>(R.id.buttonRecommend2).setOnClickListener {
-
-            }
-
-            viewPager_card = findViewById(R.id.cardNews)
-            viewPager_card.adapter = ViewPagerAdapter(getCardNews()) // 어댑터 생성
-            viewPager_card.orientation = ViewPager2.ORIENTATION_HORIZONTAL // 방향을 가로로
         }
+
+        val sortedCarbo = menus.sortedByDescending { it.carbo!! }
+        val sortedProtein = menus.sortedByDescending { it.protein!! }
+        val sortedFat = menus.sortedByDescending { it.fat!! }
+
+        if (needNutrient == "탄수화물") {
+            findViewById<Button>(R.id.buttonRecommend1).text = sortedCarbo[0].menuname
+            findViewById<Button>(R.id.buttonRecommend2).text = sortedCarbo[1].menuname
+        }else if (needNutrient == "단백질") {
+            findViewById<Button>(R.id.buttonRecommend1).text = sortedProtein[0].menuname
+            findViewById<Button>(R.id.buttonRecommend2).text = sortedProtein[1].menuname
+        }else if (needNutrient == "지방") {
+            findViewById<Button>(R.id.buttonRecommend1).text = sortedFat[0].menuname
+            findViewById<Button>(R.id.buttonRecommend2).text = sortedFat[1].menuname
+        }
+
+        findViewById<ImageButton>(R.id.backBtn).setOnClickListener {
+            intent = Intent(this, CompleteActivity::class.java)
+            startActivity(intent)
+        }
+
+        findViewById<ImageButton>(R.id.mypageBtn).setOnClickListener {
+            intent = Intent(this, MyPageActivity::class.java)
+            startActivity(intent)
+        }
+
+        findViewById<ImageButton>(R.id.homeBtn).setOnClickListener {
+            intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        // 해당 가맹점 링크
+        findViewById<ImageButton>(R.id.buttonRecommend1).setOnClickListener {
+
+        }
+
+        // 해당 가맹점 링크
+        findViewById<ImageButton>(R.id.buttonRecommend2).setOnClickListener {
+
+        }
+
+        viewPager_card = findViewById(R.id.cardNews)
+        viewPager_card.adapter = ViewPagerAdapter(getCardNews()) // 어댑터 생성
+        viewPager_card.orientation = ViewPager2.ORIENTATION_HORIZONTAL // 방향을 가로로
     }
 
     private fun getCardNews(): ArrayList<Int> {
---
+++ app/src/main/java/smu/it/ips2/MenuList.kt
@@ -0,0 +1,8 @@
+package smu.it.ips2
+
+data class MenuList(
+    val menuname: String? = "",
+    val carbo: Double?,
+    val protein: Double?,
+    val fat: Double?
+)
