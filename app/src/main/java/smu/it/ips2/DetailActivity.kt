b7e3bfc4fdb0b2b56d0d29eaa89ca9cdab795fdb
Siwon Jeon <siwonjeon234@gmail.com>
2023-05-25 02:25:07 +0900

    가맹점 추천 수정중

--- app/src/main/java/smu/it/ips2/DetailActivity.kt
+++ app/src/main/java/smu/it/ips2/DetailActivity.kt
@@ -2,17 +2,17 @@ package smu.it.ips2
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
+import android.util.Log
 import android.widget.ImageButton
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager2.widget.ViewPager2
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.QuerySnapshot
 
 class DetailActivity : AppCompatActivity() {
 
     lateinit var viewPager_card: ViewPager2
-    lateinit var firestore: FirebaseFirestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +20,7 @@ class DetailActivity : AppCompatActivity() {
 
         val needNutrient = intent.getStringExtra("nutrient")
         findViewById<TextView>(R.id.textViewNutrient).text = needNutrient
-        findViewById<TextView>(R.id.textViewNoticeNutrient).text = needNutrient
+        //findViewById<TextView>(R.id.textViewNoticeNutrient).text = needNutrient
 
         val noticeMessage = intent.getStringExtra("nutIsNeed")
         if (noticeMessage == "부족해요!") {
@@ -33,92 +33,78 @@ class DetailActivity : AppCompatActivity() {
 
         // findViewById<TextView>(R.id.textViewNotice).text = "메뉴 둘러보기"
 
-        firestore = FirebaseFirestore.getInstance()
-        //파이어스토어 메뉴명&영양성분 불러와서 리스트로 접근
-        val carbos = ArrayList<CarboList>()
-        val proteins = ArrayList<ProteinList>()
-        val fats = ArrayList<FatList>()
-        val sugars = ArrayList<SugarList>()
-        val sodiums = ArrayList<SodiumList>()
-        firestore.collection("restaurantBook")
-            .addSnapshotListener { querySnapshot, firebaseFirestoreException ->
-                for (snapshot in querySnapshot!!.documents) {
-                    var menuname = snapshot.getString("foodname")
-                    var carbo = snapshot.getDouble("carbo")
-                    var protein = snapshot.getDouble("protein")
-                    var fat = snapshot.getDouble("fat")
-                    var sugar = snapshot.getDouble("sugar")
-                    var sodium = snapshot.getDouble("sodium")
 
-                    menuname?.let { carbos.add(CarboList(it, carbo)) }
-                    menuname?.let { proteins.add(ProteinList(it, protein)) }
-                    menuname?.let { fats.add(FatList(it, fat)) }
-                    menuname?.let { sugars.add(SugarList(it, sugar)) }
-                    menuname?.let { sodiums.add(SodiumList(it, sodium)) }
+        val standard = intent.getDoubleExtra("standard", 0.0) // 비교할 standard 값
+
+        var minDifference = Double.MAX_VALUE
+        var closestFood = ""
+        var closestRes = ""
+
+        val db = FirebaseFirestore.getInstance()
+        val collectionRef = db.collection("restaurantbook")
+
+        collectionRef.get()
+            .addOnSuccessListener { querySnapshot ->
+                // 성공적으로 데이터를 가져온 경우
+                for (document in querySnapshot) {
+                    val item = document.getString("foodname")
+                    Log.d("foodname", item.toString())
+                    val fieldValue = when (needNutrient) {
+                        "탄수화물" -> document.getDouble("carbo")!!
+                        "단백질" -> document.getDouble("protein")!!
+                        "지방" -> document.getDouble("fat")!!
+                        "당류" -> document.getDouble("sugars")!!
+                        "나트륨" -> document.getDouble("sodium")!!
+                        else -> null
+                    }
+
+                    if (fieldValue != null) {
+                        val difference = Math.abs(fieldValue - standard)
+                        if (difference < minDifference) {
+                            minDifference = difference
+                            closestFood = document.getString("foodname") ?: ""
+                            closestRes = document.getString("brand") ?: ""
+                        }
+                    }
                 }
 
-                val sortedCarbo = carbos.sortedByDescending { it.carbo!! }
-                val sortedProtein = proteins.sortedByDescending { it.protein!! }
-                val sortedFat = fats.sortedByDescending { it.fat!! }
-                val sortedSugar = sugars.sortedByDescending { it.sugar!! }
-                val sortedSodium = sodiums.sortedByDescending { it.sodium!! }
+                Log.d("TAG", "Closest foodname: $closestFood")
+                Log.d("TAG", "Closest brand: $closestRes")
 
-//                if (needNutrient == "탄수화물") {
-//                    findViewById<Button>(R.id.buttonRecommend1).text =
-//                        sortedCarbo.getOrNull(0)?.menuname
-//                    findViewById<Button>(R.id.buttonRecommend2).text =
-//                        sortedCarbo.getOrNull(1)?.menuname
-//                } else if (needNutrient == "단백질") {
-//                    findViewById<Button>(R.id.buttonRecommend1).text =
-//                        sortedProtein.getOrNull(0)?.menuname
-//                    findViewById<Button>(R.id.buttonRecommend2).text =
-//                        sortedProtein.getOrNull(1)?.menuname
-//                } else if (needNutrient == "지방") {
-//                    findViewById<Button>(R.id.buttonRecommend1).text =
-//                        sortedFat.getOrNull(0)?.menuname
-//                    findViewById<Button>(R.id.buttonRecommend2).text =
-//                        sortedFat.getOrNull(1)?.menuname
-//                } else if (needNutrient == "당") {
-//                    findViewById<Button>(R.id.buttonRecommend1).text =
-//                        sortedSugar.getOrNull(0)?.menuname
-//                    findViewById<Button>(R.id.buttonRecommend2).text =
-//                        sortedSugar.getOrNull(1)?.menuname
-//                } else if (needNutrient == "나트륨") {
-//                    findViewById<Button>(R.id.buttonRecommend1).text =
-//                        sortedSodium.getOrNull(0)?.menuname
-//                    findViewById<Button>(R.id.buttonRecommend2).text =
-//                        sortedSodium.getOrNull(1)?.menuname
-//                }
-//        }.addOnFailureListener { exception ->
-//            // 오류 처리 코드
-//        }
-
-                findViewById<ImageButton>(R.id.backBtn).setOnClickListener {
-                    intent = Intent(this, SearchRestaurantActivity::class.java)
-                    startActivity(intent)
-                }
-
-                findViewById<ImageButton>(R.id.mypageBtn).setOnClickListener {
-                    intent = Intent(this, MyPageActivity::class.java)
-                    startActivity(intent)
-                }
-
-                findViewById<ImageButton>(R.id.homeBtn).setOnClickListener {
-                    intent = Intent(this, MainActivity::class.java)
-                    startActivity(intent)
-                }
-                findViewById<ImageButton>(R.id.checkBtn).setOnClickListener {
-                    intent = Intent(this, MainActivity::class.java)
-                    startActivity(intent)
-                }
-
-
-                viewPager_card = findViewById(R.id.cardNews)
-                viewPager_card.adapter = ViewPagerAdapter(getCardNews(needNutrient)) // 어댑터 생성
-                viewPager_card.orientation = ViewPager2.ORIENTATION_HORIZONTAL // 방향을 가로로
+                findViewById<TextView>(R.id.restaurantName).text = closestRes
+                findViewById<TextView>(R.id.foodName).text = closestFood
+            }
+            .addOnFailureListener { exception ->
+                // 데이터 가져오기 실패한 경우
+                Log.e("TAG", "Error getting documents: ", exception)
             }
 
+
+        findViewById<ImageButton>(R.id.backBtn).setOnClickListener {
+            intent = Intent(this, SearchRestaurantActivity::class.java)
+            startActivity(intent)
+        }
+
+        findViewById<ImageButton>(R.id.mypageBtn).setOnClickListener {
+            intent = Intent(this, MyPageActivity::class.java)
+            startActivity(intent)
+        }
+
+        findViewById<ImageButton>(R.id.homeBtn).setOnClickListener {
+            intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+        findViewById<ImageButton>(R.id.checkBtn).setOnClickListener {
+            intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+
+        viewPager_card = findViewById(R.id.cardNews)
+        viewPager_card.adapter = ViewPagerAdapter(getCardNews(needNutrient)) // 어댑터 생성
+        viewPager_card.orientation = ViewPager2.ORIENTATION_HORIZONTAL // 방향을 가로로
     }
+
 
     private fun getCardNews(needNutrient: String?): ArrayList<Int> {
         return when (needNutrient) {
